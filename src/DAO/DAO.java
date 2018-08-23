03b08f47110b01cf85187aae9478a63dcf7bb77d
Shinnnosuke Kubo <nosuke02127085.2@gmail.com>
2018-08-23 10:48:34 +0900

    DAOにListDaoの実装をした。

--- src/DAO/DAO.java
+++ src/DAO/DAO.java
@@ -1,5 +1,80 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import DTO.DTO;
+
 public class DAO {
+	public static ArrayList<DTO> listDao() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<DTO> householdlist = new ArrayList<DTO>();
+
+		try {
+			// ②JDBCドライバをロードする
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// ③DBMSとの接続を確立する
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/householdacountbook?useSSL=false",
+					"root",
+					"nosuke02127085");
+			// ④SQL文を作成する
+			String sql = "SELECT * FROM householdacounts;";
+			// ⑤SQL文を実行するための準備を行う
+			pstmt = con.prepareStatement(sql);
+
+			// ⑥SQL文を実行してDBMSから結果を受信する
+			rs = pstmt.executeQuery();
+
+			// ⑦実行結果を含んだインスタンスからデータを取り出す
+			while(rs.next()){
+				int id = rs.getInt("id");
+				String status = rs.getString("status");
+				int amount = rs.getInt("amount");
+				householdlist.add(new DTO(id,status,amount));
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBCドライバが見つかりません。");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DBアクセス時にエラーが発生しました。");
+			e.printStackTrace();
+		} finally {
+			// ⑧DBMSから切断する
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+		}
+		return householdlist;
+	}
 
 }
