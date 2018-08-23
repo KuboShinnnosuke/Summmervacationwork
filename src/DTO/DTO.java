6183beb792cc3070dd7ce1d1e36dc62816c15ae6
Shinnnosuke Kubo <nosuke02127085.2@gmail.com>
2018-08-23 10:08:39 +0900

    DTOを作成。SQLは必須課題に加え、追加課題である「合計の表示」までを目標をしたものを作成することにした。そのために必要な項目statusとamountをカラムとして持つテーブル[householdacounts]を利用する。

--- src/DTO/DTO.java
+++ src/DTO/DTO.java
@@ -1,5 +1,34 @@
 package DTO;
 
 public class DTO {
+	private int id;
+	private String status;
+	private int amount;
+
+	public DTO(int id,String status,int amount){
+		this.id=id;
+		this.status=status;
+		this.amount=amount;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
 
 }
